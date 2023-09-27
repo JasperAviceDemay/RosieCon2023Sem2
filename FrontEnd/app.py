da8bbbbe19ff3964ef03b04de2ad145e2e6beaaf
Alli-st <s3843971@student.rmit.edu.au>
2023-09-27 15:56:37 +1000

    Moved text generation into separate file

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -4,17 +4,13 @@ import html
 import azure.cognitiveservices.speech as speechsdk
 from gtts import gTTS
 from flask import Flask, request, jsonify, render_template, send_file
+from rosieTextGen import textGen
 
 app = Flask(__name__)
-textGenHost = "localhost:5000"
 AzureSpeechKey, AzureSpeechRegion = os.environ.get('AZURESPEECHKEY'), os.environ.get('AZUREREGION')
 speech_config = speechsdk.SpeechConfig(subscription=AzureSpeechKey, region=AzureSpeechRegion)
 lastResponse = None
 recognisedText = None
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 def transcribeFile(audio_output_file_path):
     audio_config = speechsdk.AudioConfig(filename=audio_output_file_path)
@@ -34,69 +30,15 @@ def transcribeFile(audio_output_file_path):
             print("Error details: {}".format(cancellation_details.error_details))
             print("Did you set the speech resource key and region values?")
 
-def textGen(input):
-    textgenURL = f"http://{textGenHost}/api/v1/chat"
-    request = {
-        'user_input': input,
-        'max_new_tokens': 250,
-        'auto_max_new_tokens': False,
-        'max_tokens_second': 0,
-        'mode': 'chat',  # Valid options: 'chat', 'chat-instruct', 'instruct'
-        'character': 'Rosie',
-        'instruction_template': 'Vicuna-v1.1',  # Will get autodetected if unset
-        'your_name': 'You',
-        'regenerate': False,
-        '_continue': False,
-        'chat_instruct_command': 'Continue the chat dialogue below. Write a single reply for the character "<|character|>".\n\n<|prompt|>',
-
-        # Generation params. If 'preset' is set to different than 'None', the values
-        # in presets/preset-name.yaml are used instead of the individual numbers.
-        'preset': 'None',
-        'do_sample': True,
-        'temperature': 0.7,
-        'top_p': 0.1,
-        'typical_p': 1,
-        'epsilon_cutoff': 0,  # In units of 1e-4
-        'eta_cutoff': 0,  # In units of 1e-4
-        'tfs': 1,
-        'top_a': 0,
-        'repetition_penalty': 1.18,
-        'repetition_penalty_range': 0,
-        'top_k': 40,
-        'min_length': 0,
-        'no_repeat_ngram_size': 0,
-        'num_beams': 1,
-        'penalty_alpha': 0,
-        'length_penalty': 1,
-        'early_stopping': False,
-        'mirostat_mode': 0,
-        'mirostat_tau': 5,
-        'mirostat_eta': 0.1,
-        'guidance_scale': 1,
-        'negative_prompt': '',
-
-        'seed': -1,
-        'add_bos_token': True,
-        'truncation_length': 2048,
-        'ban_eos_token': False,
-        'custom_token_bans': '',
-        'skip_special_tokens': True,
-        'stopping_strings': []
-    }
-
-    print("Generating...")
-    response = requests.post(textgenURL, json=request)
-    print(response.status_code)
-    if response.status_code == 200:
-        result = html.unescape(response.json()['results'][0]['history']['visible'][-1][1])
-        print(result)
-        lastResponse = result
-        return result
-    
 def textToSpeech(text):
     language = request.args.get('lang', 'en')
     tts = gTTS(text=text, lang=language)
     tts.save("static/uploads/speak.wav")
+
+# Flask Routing
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route("/get-last-response")
 def getLastResponse():
@@ -134,7 +76,7 @@ def SpeechToText():
         global recognisedText
         recognisedText = transcription
         print(transcription)
-        response = textGen(transcription)
+        response = rosieText.textGen(transcription)
         if response is not None:
             textToSpeech(response)
     
@@ -160,5 +102,6 @@ def text_to_speech():
     return jsonify({"response": "Audio file saved successfully"}), 200
 
 if __name__ == '__main__':
+    rosieText = textGen()
     app.run(port=5001)
 
---
+++ FrontEnd/rosieTextGen.py
@@ -0,0 +1,66 @@
+import requests
+import html
+
+class textGen:
+    def __init__(self):
+        self.textGenHost="localhost:5000"
+        self.lastResponse = None
+
+    def textGen(self, input):
+        request = {
+            'user_input': input,
+            'max_new_tokens': 250,
+            'auto_max_new_tokens': False,
+            'max_tokens_second': 0,
+            'mode': 'chat',  # Valid options: 'chat', 'chat-instruct', 'instruct'
+            'character': 'Rosie',
+            'instruction_template': 'Vicuna-v1.1',  # Will get autodetected if unset
+            'your_name': 'You',
+            'regenerate': False,
+            '_continue': False,
+            'chat_instruct_command': 'Continue the chat dialogue below. Write a single reply for the character "<|character|>".\n\n<|prompt|>',
+
+            # Generation params. If 'preset' is set to different than 'None', the values
+            # in presets/preset-name.yaml are used instead of the individual numbers.
+            'preset': 'None',
+            'do_sample': True,
+            'temperature': 0.7,
+            'top_p': 0.1,
+            'typical_p': 1,
+            'epsilon_cutoff': 0,  # In units of 1e-4
+            'eta_cutoff': 0,  # In units of 1e-4
+            'tfs': 1,
+            'top_a': 0,
+            'repetition_penalty': 1.18,
+            'repetition_penalty_range': 0,
+            'top_k': 40,
+            'min_length': 0,
+            'no_repeat_ngram_size': 0,
+            'num_beams': 1,
+            'penalty_alpha': 0,
+            'length_penalty': 1,
+            'early_stopping': False,
+            'mirostat_mode': 0,
+            'mirostat_tau': 5,
+            'mirostat_eta': 0.1,
+            'guidance_scale': 1,
+            'negative_prompt': '',
+
+            'seed': -1,
+            'add_bos_token': True,
+            'truncation_length': 2048,
+            'ban_eos_token': False,
+            'custom_token_bans': '',
+            'skip_special_tokens': True,
+            'stopping_strings': []
+        }
+
+        print("Generating...")
+        response = requests.post(textGenUrl = f"http://{self.textGenHost}/api/v1/chat", json=request)
+        print(response.status_code)
+        if response.status_code == 200:
+            result = html.unescape(response.json()['results'][0]['history']['visible'][-1][1])
+            print(result)
+            self.lastResponse = result
+            return result
+        
