ef1670ba0f1accc9990406c3822442384b874ba5
JasperAviceDemay <112991036+JasperAviceDemay@users.noreply.github.com>
2023-11-30 12:26:02 +1100

    Update stop.py

--- stop.py
+++ stop.py
@@ -1,10 +1,13 @@
 import os
 import requests
+import time
 
 url = "https://rp0kq2egtk.execute-api.ap-southeast-2.amazonaws.com/control/stop"
 headers = {
     'auth': os.environ['APIAUTH']
 }
+
+time.sleep(30)
 
 response = requests.request("POST", url, headers=headers)
 
