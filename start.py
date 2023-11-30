1f42a9333e07459727fc3bda03cd9172aa13db4f
JasperAviceDemay <112991036+JasperAviceDemay@users.noreply.github.com>
2023-11-30 16:14:25 +1100

    Update start.py

--- start.py
+++ start.py
@@ -7,9 +7,7 @@ headers = {
     'auth': os.environ['APIAUTH']
 }
 
-
-response = requests.request
-(while True:
+while True:
     try:
         response = requests.request("POST", url, headers=headers)
         if response.status_code >= 200 and response.status_code < 300:
@@ -17,9 +15,7 @@ response = requests.request
             break
         else:
             print(f"Request failed with status {response.status_code}, retrying...")
-            time.sleep(20)  # wait for 5 seconds before retrying
+            time.sleep(20)  # wait for 20 seconds before retrying
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}, retrying...")
-        time.sleep(20)  "POST", url, headers=headers)
-
-print(response.text)
+        time.sleep(20)  # wait for 20 seconds before retrying
