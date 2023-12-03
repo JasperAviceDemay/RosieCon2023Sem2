fb51596232dcf9533ba800db897eab275fb3c10b
JasperAviceDemay <112991036+JasperAviceDemay@users.noreply.github.com>
2023-12-04 09:38:56 +1100

    Update stop.py

--- stop.py
+++ stop.py
@@ -6,10 +6,15 @@ url = "https://rp0kq2egtk.execute-api.ap-southeast-2.amazonaws.com/control/stop"
 headers = {
     'auth': os.environ['APIAUTH']
 }
-
-time.sleep(60)
-
-response = requests.request("POST", url, headers=headers)
-
-print(response.text)
- 
+while True:
+    try:
+        response = requests.request("POST", url, headers=headers)
+        if response.status_code >= 200 and response.status_code < 300:
+            print(response.text)
+            break
+        else:
+            print(f"Request failed with status {response.status_code}, retrying...")
+            time.sleep(20)  # wait for 20 seconds before retrying
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}, retrying...")
+        time.sleep(20)  # wait for 20 seconds before retrying
