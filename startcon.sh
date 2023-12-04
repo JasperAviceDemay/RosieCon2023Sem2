f74e10bfc0248fe1d055245e58fce585205105a5
JasperAviceDemay <112991036+JasperAviceDemay@users.noreply.github.com>
2023-12-04 13:18:25 +1100

    Update startcon.sh

--- startcon.sh
+++ startcon.sh
@@ -10,11 +10,7 @@ sleep 30  # waits for 30 seconds
 # Start the SSH connection in the background
 nohup ssh -tt -i /root/.ssh/rosiekey.pem -o UserKnownHostsFile=/root/.ssh/rosiekeyhost ubuntu@ec2-52-62-118-55.ap-southeast-2.compute.amazonaws.com > /rosiecon/RosieCon2023Sem2/rosie.out & disown
 
-# Wait for the SSH connection to be established
-while ! ssh -q -o BatchMode=yes -o ConnectTimeout=5 ubuntu@ec2-52-62-118-55.ap-southeast-2.compute.amazonaws.com exit; do
-    echo "Waiting for SSH connection..."
-    sleep 20
-done
+sleep 20
 
 # Continue with the rest of the script
 echo "SSH connection established, continuing with script..."
