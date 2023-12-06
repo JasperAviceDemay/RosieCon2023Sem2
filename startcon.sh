5696c47d7cc4d5290f699bc2f8dc8f1a6e2a1de3
JasperAviceDemay <112991036+JasperAviceDemay@users.noreply.github.com>
2023-12-07 09:52:52 +1100

    Update startcon.sh

--- startcon.sh
+++ startcon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Add your commands to .bashrc
+# start the ec2 machine using api 
 python3 /rosiecon/RosieCon2023Sem2/start.py
 
 # Add a delay
@@ -10,7 +10,13 @@ sleep 30  # waits for 30 seconds
 # Start the SSH connection in the background
 nohup ssh -tt -i /root/.ssh/rosiekey.pem -o UserKnownHostsFile=/root/.ssh/rosiekeyhost ubuntu@ec2-52-62-118-55.ap-southeast-2.compute.amazonaws.com > /rosiecon/RosieCon2023Sem2/rosie.out & disown
 
-sleep 20
+sleep 120
 
 # Continue with the rest of the script
 echo "SSH connection established, continuing with script..."
+
+#start speech to text 
+
+python3 /rosiecon/RosieCon2023Sem2/realtime.py
+
+sleep 20 
